23d1115c057fbe73f9b3c50430e41474fceff47c
Aenge <scottthomas07@gmail.com>
2019-11-27 22:04:11 -0500

    improve compile script to not include any .java files when copying src folder

--- compile.bat
+++ compile.bat
@@ -3,5 +3,7 @@ dir /s /b src\*.java > sources.txt & javac --module-path "%PATH_TO_FX_MODS%;lib"
 rmdir "out/OpenRSCSpriteTool" /s /q
 jlink --module-path "%PATH_TO_FX_MODS%;lib;out/mods" --add-modules spritetool --output out/OpenRSCSpriteTool
 copy "properscript" "out/OpenRSCSpriteTool/RUN.bat"
-xcopy /E /I "src" "out/OpenRSCSpriteTool/src"
-xcopy /E /I "resource" "out/OpenRSCSpriteTool/resource"
+xcopy /E /I /S "src" "out/OpenRSCSpriteTool/src" /EXCLUDE:excluded.txt
+xcopy /E /I /S "resource" "out/OpenRSCSpriteTool/resource"
+cd out/OpenRSCSpriteTool/src
+for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do rd "%%d"
